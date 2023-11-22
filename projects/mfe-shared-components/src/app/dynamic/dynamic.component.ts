cd034d653eaaf1ef85f0eb88f129d6913d0e5f15
Sayantan <web.dev.sayantan@gmail.com>
2023-11-22 11:35:33 +0530

    lib component now takes inputs

--- projects/mfe-shared-components/src/app/dynamic/dynamic.component.ts
+++ projects/mfe-shared-components/src/app/dynamic/dynamic.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'mfe-shared-dynamic',
   templateUrl: './dynamic.component.html',
-  styleUrls: ['./dynamic.component.css']
+  styleUrls: ['./dynamic.component.css'],
 })
 export class DynamicComponent {
-
+  @Input() propOne!: string;
+  @Input() propTwo!: number;
 }
