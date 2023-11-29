4439ccd508fd51eefce90ac39fc876524428786b
Sayantan <web.dev.sayantan@gmail.com>
2023-11-29 22:15:45 +0530

    removed unused project

--- projects/mfe-shared-components/src/app/app.module.ts
+++ projects/mfe-shared-components/src/app/app.module.ts
@@ -2,13 +2,12 @@ import { Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SharedComponentsModule } from '../../../shared-components/src/public-api';
 import { DynamicComponent } from './dynamic/dynamic.component';
 import { createCustomElement } from '@angular/elements';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedComponentsModule],
+  imports: [BrowserModule],
   providers: [],
 })
 export class AppModule {
--- projects/shared-components/src/lib/shared-components.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'shared-shared-components',
-  template: ` <p>shared-components works!</p> `,
-  styles: [],
-})
-export class SharedComponentsComponent {}
